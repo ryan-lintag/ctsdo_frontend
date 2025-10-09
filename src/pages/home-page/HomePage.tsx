4e4ba3d07ddaf1fe358d8098d98384dae0cd5ca2
ryan.lintag@excentone.com <ryan.lintag@excentone.com>
2025-10-09 13:02:19 +0800

    Updated the map

--- src/pages/home-page/HomePage.tsx
+++ src/pages/home-page/HomePage.tsx
@@ -83,7 +83,7 @@ const HomePage = () => {
         <h2 className="mb-4">Our Location</h2>
       </Col>
 <iframe
-  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30814.727311615177!2d120.50327758173422!3d15.11209407047506!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33968cbf29ec7ebb%3A0x66f07782278c9b64!2sDolores%20(Hacienda%20Dolores)%2C%20Porac%2C%20Pampanga%2C%20Philippines!5e0!3m2!1sen!2sph"
+  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2646.522613585975!2d120.54483323047396!3d15.080216561036277!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3396f4bb560fd10b%3A0xd96b63f6379b3384!2sPorac%20Manpower%20Training%20Center!5e0!3m2!1sen!2ssg!4v1759986070976!5m2!1sen!2ssg"
   width="100%"
   height="450"
   style={{ border: 0 }}
